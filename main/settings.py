f7d958ac32bc1ee6642b9b8c20b81daf11f82c2f
Kudakwashe Marongedza <107895856+Ashemaro@users.noreply.github.com>
2024-12-21 10:51:00 +0200

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'main/staticfiles'),
+    os.path.join(BASE_DIR, 'main/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'main/staticfiles')
 
